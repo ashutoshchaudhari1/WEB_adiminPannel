da5c04d4e933a2b6c5c7c7fafb4330c82417bc63
ashutosh <ashu.4chaudhari@gmail.com>
2021-07-22 04:01:10 +0530

    update

---
+++ lib/constants/controllers.dart
@@ -0,0 +1,5 @@
+import 'package:webadminpannel/controllers/menu_controller.dart';
+import 'package:webadminpannel/controllers/navigation_controller.dart';
+
+MenuController menuController = MenuController.instance;
+NavigationController navigationController = NavigationController.instance;
